2452d8944286d2f1cf9abd9c27e95329eb4095dd
vikas yadav <thejsdeveloper@gmail.com>
2024-02-02 12:23:39 +0530

    feat: adding theme constants

---
+++ constants/theme/index.ts
@@ -0,0 +1,13 @@
+import { colors } from "./colors";
+import { sizes } from "./size";
+import { lineHeights, space } from "./space";
+import { fontSizes, fontWeights } from "./fonts";
+
+export const theme = {
+  colors,
+  space,
+  lineHeights,
+  sizes,
+  fontSizes,
+  fontWeights,
+};
