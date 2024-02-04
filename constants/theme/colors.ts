d7d7bcbc95db8450ad07de692fbe48571ef61f97
vikas yadav <thejsdeveloper@gmail.com>
2024-02-04 19:05:19 +0530

    chore: removing unused color vars

--- constants/theme/colors.ts
+++ constants/theme/colors.ts
@@ -8,10 +8,6 @@ export const colors = {
     primary: "#262626",
     secondary: "#757575",
     tertiary: "#F1F1F1",
-    quaternary: "#FFFFFF",
-    disabled: "#DEDEDE",
-    error: "#D0421B",
-    success: "#138000",
   },
   bg: {
     primary: "#FFFFFF",
@@ -22,9 +18,6 @@ export const colors = {
     primary: "#262626",
     secondary: "#757575",
     disabled: "#9C9C9C",
-    inverse: "#FFFFFF",
-    error: "#D0421B",
-    success: "#138000",
     link: "#2e78b7",
   },
 };
